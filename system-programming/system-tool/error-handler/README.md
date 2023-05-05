13794b5473ee4d7323e407f90c3bf4ee991cd532
pythonstrup <pythonstrup@gmail.com>
2023-05-05 16:22:37 +0900

    error handler

---
+++ system-programming/system-tool/error-handler/README.md
@@ -0,0 +1,20 @@
+# Error Handler
+
+## Execute
+
+- perror
+
+```
+$ gcc -o perror.out perror.c
+$ ./perror.out
+```
+
+<br/>
+
+
+- strerror
+
+```
+$ gcc -o strerror.out strerror.c
+$ ./strerror.out
+```
---
+++ system-programming/system-tool/error-handler/strerror.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
+
+extern int errno;
+
+int main() {
+	char *err;
+
+	if (access("test.txt", R_OK) == -1) {
+		err = strerror(errno);
+		printf("Error!!: %s(text.txt)\n", err);
+		exit(1);
+	}
+}
+
