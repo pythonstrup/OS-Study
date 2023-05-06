0affbfe1fbc97802b777d836782a013710abc27a
pythonstrup <pythonstrup@gmail.com>
2023-05-06 16:52:06 +0900

    file-type

---
+++ system-programming/file-system/file-type/macro_search.c
@@ -0,0 +1,20 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>
+
+int main() {
+	struct stat statbuf;
+	int kind;
+
+	stat("linux.txt", &statbuf);
+
+	printf("Mode = %o\n", (unsigned int) statbuf.st_mode);
+
+	if (S_ISLNK(statbuf.st_mode))
+		printf("linux.txt: Sysbolic Link\n");
+	if (S_ISDIR(statbuf.st_mode))
+		printf("linux.txt: Driectory\n");
+	if (S_ISREG(statbuf.st_mode))
+		printf("linux.txt: Regular File\n");
+}
+
