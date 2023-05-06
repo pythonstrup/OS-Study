502156f10f5561cbaa4279461098760218532859
pythonstrup <pythonstrup@gmail.com>
2023-05-06 17:24:36 +0900

    file link

---
+++ system-programming/file-system/link/realpath.c
@@ -0,0 +1,11 @@
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int main() {
+	char buf[BUFSIZ];
+
+	realpath("linux.sym", buf);
+	printf("linux.sym: REALPATH = %s\n",buf);
+}
+
---
+++ system-programming/file-system/link/symlink.c
@@ -0,0 +1,6 @@
+#include <unistd.h>
+
+int main() {
+	symlink("linux.txt", "linux.sym");
+}
+
