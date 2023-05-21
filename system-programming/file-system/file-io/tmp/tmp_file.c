9bc0e136d5007caa75a633fdcb33698729237667
pythonstrup <pythonstrup@gmail.com>
2023-05-21 16:06:01 +0900

    temp file create

---
+++ system-programming/file-system/file-io/tmp/tmp_file.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+	char *fname;
+	char fntmp[BUFSIZ];
+	char template[32];
+
+	fname = tmpnam(NULL);
+	printf("1. TMP File Name(tmpnam): %s\n", fname);
+
+	tmpnam(fntmp);
+	printf("2. TMP File Name(tmpnam): %s\n", fntmp);
+
+	strcpy(template, "/tmp/bellXXXXXX");
+	fname = mktemp(template);
+	printf("3. TMP File Name(mktemp): %s\n", fname);
+}
+
