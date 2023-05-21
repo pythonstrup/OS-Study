108e656ef49b4cba63f684c5a8b8a8937fecefb2
pythonstrup <pythonstrup@gmail.com>
2023-05-21 15:49:12 +0900

    High Level File I/O

---
+++ system-programming/file-system/file-io/high-level-io/format_based_input.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main() {
+	FILE *rfp;
+	int id, s1, s2, s3, s4, n;
+
+	if ((rfp = fopen("linux.dat", "r")) == NULL) {
+		perror("fopen: linux.dat");
+		exit(1);
+	}
+
+	printf("ID  Score\n");
+	while ((n=fscanf(rfp, "%d %d %d %d %d", &id, &s1, &s2, &s3, &s4)) != EOF) {
+		printf("%d: %d\n", id, (s1+s2+s3+s4)/4);
+	}
+}
+
---
+++ system-programming/file-system/file-io/high-level-io/format_based_output.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main() {
+	FILE *rfp, *wfp;
+	int id, s1, s2, s3, s4, n;
+
+	if ((rfp = fopen("linux.dat", "r")) == NULL) {
+		perror("fopen: linux.dat");
+		exit(1);
+	}
+
+	if ((wfp = fopen("linux.scr", "w")) == NULL) {
+		perror("fopen: linux.scr");
+		exit(1);
+	}
+
+	fprintf(wfp, "ID  Score\n");
+	while ((n=fscanf(rfp, "%d %d %d %d %d", &id, &s1, &s2, &s3, &s4)) != EOF) {
+		fprintf(wfp, "%d: %d\n", id, (s1+s2+s3+s4)/4);
+	}
+
+	fclose(rfp);
+	fclose(wfp);
+}
+
